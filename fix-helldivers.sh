a909e2207711fd127b347cf4d17e8c0a5a7e6e27
YourBoyRory <yourboyrory@gmail.com>
2024-03-12 23:05:28 -0400

    final commit

---
+++ fix-helldivers.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sed -i '0,/fullscreen = true/{s/fullscreen = true/fullscreen = false/}' /opt/steam/steamapps/compatdata/553850/pfx/drive_c/users/steamuser/AppData/Roaming/Arrowhead/Helldivers2/user_settings.config
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo cp ./fix-helldivers.sh /usr/local/bin/fix-helldivers
+sudo chmod 755 /usr/local/bin/fix-helldivers
